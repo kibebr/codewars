219e69294a9e2662a3d6c90e8dcd4abd7450f7c7
kibe <47016873+kibebr@users.noreply.github.com>
2019-11-16 15:22:29 -0500

    Removed unnecessary code

--- c/shortest_word.c
+++ c/shortest_word.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
 #include <sys/types.h>
-#include <string.h>
-
-ssize_t find_short(const char *s);
-
-int main(void){
-	printf("%zi\n", find_short("bitcoin take over the world maybe who knows perhaps"));
-	printf("%zi\n", find_short("lets talk about C the best language"));
-}
 
 ssize_t find_short(const char *s){
 	ssize_t shortest = 0, curr = 0;
