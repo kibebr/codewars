6eca8eea2d6c9fd53a57dbc9da3b8aa6caa1fe51
kibe <47016873+kibebr@users.noreply.github.com>
2019-11-16 16:14:48 -0500

    Improved Kata "Shortest Word"

--- c/shortest_word.c
+++ c/shortest_word.c
@@ -3,22 +3,18 @@
 ssize_t find_short(const char *s){
 	ssize_t shortest = 0, curr = 0;
 
-	while(1){
-		if(*s != '\0' && *s != ' ')
+	do{
+		if(*s != ' ')
 			++curr;
 		else{
 			if(curr == 1) 
-				return curr;
+				return curr; // if the length of the word is 1... then just return 1. can't be bigger than that
 			else if(curr <= shortest || shortest == 0)
 				shortest = curr;			
-			if(*s == '\0')
-				break;
-			
+
 			curr = 0;
 		}
-		
-		s += sizeof(char);
-	}
+	}while((s += sizeof(char)) && (*s != '\0'));
 
 	return shortest;
 }
