cf979fcc238f7c5207ac379a314b9ad0b55f5419
kibe <47016873+kibebr@users.noreply.github.com>
2019-11-16 20:15:48 -0500

    Updated Kata "Exes and Os"

--- c/exes_and_ohs.c
+++ c/exes_and_ohs.c
@@ -1,28 +1,17 @@
 #include <stdbool.h>
-#include <stdio.h>
-
-bool xo(const char*);
-
-int main(void){
-	if(xo("xxxooo"))
-		printf("same\n");
-}
 
 bool xo (const char* str)
 {
-	unsigned int exes = 0, ohs = 0;
+    unsigned int exes = 0, ohs = 0;
+    do{
+        if(*str == 'x' || *str == 'X')
+	    ++exes;
+        else
+            ++ohs;
+	}while((str += sizeof(char)) && (*str != '\0'));
 
-	while(*str != '\0'){
-		if(*str == 'x' || *str == 'X')
-			++exes;
-		else // assumes other characters are 'o's
-			++ohs;
-			
-		str += sizeof(char);
-	}
-
-	if(exes == ohs)
-		return true;
-	else
-		return false;
+    if(exes == ohs) 
+        return true;
+    else
+	return false;
 }
