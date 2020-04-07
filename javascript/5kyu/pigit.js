84ebd3471f8af8d5d9cacd8ff5da8dd42a3fed1c
kibe <0763722@students.dadeschools.net>
2020-04-06 22:28:09 -0400

    Added 'pigIt' kata

---
+++ javascript/5kyu/pigit.js
@@ -0,0 +1,24 @@
+/*
+ * Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+*/
+
+function pigIt(str){
+	let arr = str.split(" ");
+	let newStr = "";
+	
+
+	for(let wi = 0, len = arr.length; wi < len; ++wi){
+		if(!arr[wi].match(/[a-zA-Z]/)){
+			newStr += arr[wi];
+			continue;
+		}
+		else{
+			for(let j = 0; (rest = arr[wi][++j]); newStr += rest){}
+		}
+
+		newStr += arr[wi][0];
+		newStr += (wi === len-1) ? "ay" : "ay ";
+	}
+
+	return newStr;
+}
